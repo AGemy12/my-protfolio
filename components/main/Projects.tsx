2bdcbc1c274a473ea780e0702c2d40fe47cce582
Ahmed Gamal <aggamal98@gmail.com>
2026-02-24 16:52:59 +0200

    Fix scroll y

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -5,7 +5,7 @@ import ProjectsItem from "../sections/projects/ProjectsItem";
 export default function Projects() {
   return (
     <section
-      className="container mx-auto md:w-full w-11/12 py-16 md:py-24 lg:py-32"
+      className="container mx-auto md:w-full w-11/12 py-16 md:py-24 lg:py-32 overflow-x-hidden"
       id="projects"
     >
       <SectionTitle title="My Projects" />
